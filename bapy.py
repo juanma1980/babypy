e947aa8969089d13903b982515d0c4af2ee4e07e
juanma1980 <juanma1980@gmail.com>
2018-12-05 23:41:21 +0100

    Add clear screen bind to keyspace

--- bapy.py
+++ bapy.py
@@ -19,10 +19,10 @@ class pykids:
 		self._running=True
 		self.lastkey=None
 		info=pygame.display.Info()
-		self.weight,self.height=info.current_w,info.current_h
-		if self.weight>2047:
-			self.weight=int(self.weight/2)
-		self.size=self.weight,self.height
+		self.width,self.height=info.current_w,info.current_h
+		if self.width>2047:
+			self.width=int(self.width/2)
+		self.size=self.width,self.height
 
 		self.screen=pygame.display.set_mode(self.size)
 		self._display_surf=pygame.display.set_mode(self.size,pygame.HWSURFACE)
@@ -33,8 +33,11 @@ class pykids:
 		if event.type==pygame.KEYDOWN:
 			self.lastkey=event.unicode
 		if event.type==pygame.KEYUP:
-			self.posY=0
-			self.on_key(event)
+			if event.key == pygame.K_BACKSPACE:
+				self._clear_screen()
+			else:
+				self.posY=0
+				self.on_key(event)
 		if event.type==pygame.MOUSEBUTTONUP:
 			if event.button<4:
 				self.posY=0
@@ -42,11 +45,8 @@ class pykids:
 
 	def on_click(self,event):
 		(posx,posy)=event.pos
-		colorr,colorb,colorg=random.randint(0,255),random.randint(0,255),random.randint(0,255)
-		while colorr+colorb+colorg<50:
-			colorr,colorb,colorg=random.randint(0,255),random.randint(0,255),random.randint(0,255)
-		color=(colorr,colorb,colorg)
 		width=random.randint(0,2)
+		color=self._pick_color()
 		if event.button==4 or event.button==5:
 			width=random.randint(10,20)
 			inc=1
@@ -60,7 +60,7 @@ class pykids:
 			pygame.draw.line(self.screen,color,[posx,self.posY],[posx,self.posY+1],width)
 		else:
 			figure=random.choice(self.geom)
-			sizeX=random.randint(0,(self.weight-posx))
+			sizeX=random.randint(0,(self.width-posx))
 			sizeY=random.randint(0,(self.height-posy))
 			if figure=='rect':
 				pygame.draw.rect(self.screen,color,[posx,posy,sizeX,sizeY],width)
@@ -72,15 +72,12 @@ class pykids:
 		pygame.display.flip()
 				
 	def on_key(self,event):
-		posx=random.randint(0,self.weight)
+		posx=random.randint(0,self.width)
 		posy=random.randint(0,self.height)
-		colorr,colorb,colorg=random.randint(0,255),random.randint(0,255),random.randint(0,255)
-		while colorr+colorb+colorg<50:
-			colorr,colorb,colorg=random.randint(0,255),random.randint(0,255),random.randint(0,255)
-		color=(colorr,colorb,colorg)
 		size=random.randint(0,self.height)
-		while posx+size>self.weight or posx<0:
-			posx=random.randint(0,self.weight)
+		color=self._pick_color()
+		while posx+size>self.width or posx<0:
+			posx=random.randint(0,self.width)
 			size=random.randint(0,self.height)
 		while posy+size>self.height or posy<0:
 			posy=random.randint(0,self.height)
@@ -88,6 +85,19 @@ class pykids:
 		font=pygame.font.SysFont('roboto',size)
 		f=font.render(self.lastkey,False,color,None)
 		self._display_surf.blit(f,(posx,posy))
+		pygame.display.flip()
+
+	def _pick_color(self):
+		colorr,colorb,colorg=random.randint(0,255),random.randint(0,255),random.randint(0,255)
+		min_bright=0
+		while colorr+colorb+colorg<min_bright:
+			colorr,colorb,colorg=random.randint(0,255),random.randint(0,255),random.randint(0,255)
+		color=(colorr,colorb,colorg)
+		return(color)
+
+	def _clear_screen(self):
+		color=self._pick_color()
+		pygame.draw.rect(self.screen,color,[0,0,self.width,self.height],0)
 		pygame.display.flip()
 
 	def on_loop(self):
